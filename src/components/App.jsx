209035f54811c3394b533dcaf5f057dc43cd6373
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-15 20:15:00 +0300

    Pokemon Update files
    Pokemon Update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+import PokemonForm from './PokemonForm';
+
+export class App extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleFormSubmit = pokemonName => {
+    this.setState({ pokemonName });
+  };
+  render() {
+    return (
+      <div
+        style={{
+          maxWidth: 1170,
+          margin: '0 auto',
+          padding: 20,
+        }}
+      >
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+}
---
+++ src/components/PokemonForm.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import { ImSearch } from 'react-icons/im';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const styles = {
+  form: {
+    marginBottom: 20,
+  },
+};
+
+export default class PokemonForm extends Component {
+  state = {
+    pokemonName: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ pokemonName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.pokemonName.trim() === '') {
+      toast.error('🦄 Wow so easy!');
+      return;
+    }
+
+    this.props.onSubmit(this.state.pokemonName);
+    this.setState({ pokemonName: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} style={styles.form}>
+        <input
+          type="text"
+          name="pokemonName"
+          value={this.state.pokemonName}
+          onChange={this.handleNameChange}
+        />
+        <button type="submit">
+          <ImSearch style={{ marginRight: 8 }} />
+          Знайти
+        </button>
+      </form>
+    );
+  }
+}
